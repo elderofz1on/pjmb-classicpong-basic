46bd11f5a9fe711242c617b68823538135782d4a
Wagner <wprodriguesbr@gmail.com>
2023-05-30 14:42:34 -0300

    Atualizar 'pxt.json, main.blocks, main.ts'

--- main.ts
+++ main.ts
@@ -7,28 +7,23 @@ let pontuacao = 0
 let intervalo = 0
 let intervalo_passo = 0
 let em_jogo = false
-//  movimento para esquerda
-input.onButtonPressed(Button.A, function on_button_pressed_a() {
-    
+// movimento para esquerda
+input.onButtonPressed(Button.A, function () {
     if (raquete_x > 0) {
         led.unplot(raquete_x + 1, 4)
         raquete_x = raquete_x - 1
         led.plot(raquete_x, 4)
     }
-    
 })
-//  movimento para direita
-input.onButtonPressed(Button.B, function on_button_pressed_b() {
-    
+// movimento para direita
+input.onButtonPressed(Button.B, function () {
     if (raquete_x < 3) {
         led.unplot(raquete_x, 4)
         raquete_x = raquete_x + 1
         led.plot(raquete_x + 1, 4)
     }
-    
 })
-basic.forever(function on_forever() {
-    
+basic.forever(function () {
     raquete_x = 0
     bola_eixo_x = 3
     bola_eixo_y = 4
@@ -48,7 +43,6 @@ basic.forever(function on_forever() {
         } else if (bola_eixo_x + bola_eixo_deslocamento_x < 0) {
             bola_eixo_deslocamento_x = bola_eixo_deslocamento_x * -1
         }
-        
         if (bola_eixo_y + bola_eixo_deslocamento_y < 0) {
             bola_eixo_deslocamento_y = bola_eixo_deslocamento_y * -1
         } else if (bola_eixo_y + bola_eixo_deslocamento_y > 3) {
@@ -58,13 +52,10 @@ basic.forever(function on_forever() {
                 if (intervalo - intervalo_passo >= 0) {
                     intervalo = intervalo - intervalo_passo
                 }
-                
             } else {
                 em_jogo = false
             }
-            
         }
-        
         if (em_jogo) {
             led.plot(bola_eixo_x + bola_eixo_deslocamento_x, bola_eixo_y + bola_eixo_deslocamento_y)
             led.unplot(bola_eixo_x, bola_eixo_y)
@@ -75,6 +66,5 @@ basic.forever(function on_forever() {
             game.setScore(pontuacao)
             game.gameOver()
         }
-        
     }
 })
